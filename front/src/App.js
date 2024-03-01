2c71ccad9aa1505c0ce8b016bf94ac7fc9e5e074
Ilan Lallemand <ilan@MacBook-Pro-de-Ilan.local>
2024-03-01 17:10:55 +0100

    add routing

--- front/src/App.js
+++ front/src/App.js
@@ -1,7 +1,33 @@
+import { Home, Detail, FormAdd, FormEmail }  from './pages'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
 function App() {
   return (
     <div className="App">
-        hello
+        <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Accueil</Link>
+          </li>
+          <li>
+            <Link to="/formemail">Formulaire Contact</Link>
+          </li>
+          <li>
+            <Link to="/formadd">Ajouter une carte</Link>
+          </li>
+          <li>
+            <Link to="/detail">détail de la carte</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/detail" element={<Detail />} />
+        <Route path="/formadd" element={<FormAdd />} />
+        <Route path="/formemail" element={<FormEmail />} />
+      </Routes>
+    </Router>
     </div>
   );
 }
---
+++ front/src/pages/index.js
@@ -0,0 +1,4 @@
+export { default as Home } from './Home.jsx';
+export { default as FormAdd } from './FormAdd.jsx';
+export { default as FormEmail } from './FormEmail.jsx';
+export { default as Detail } from './Detail.jsx';
