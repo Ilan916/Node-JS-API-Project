b0f7d2b8a1e54c0ed927a9635e9432735b0238a5
Kaowarstail <victor.dane92@gmail.com>
2024-03-01 16:17:03 +0100

    fix api

--- back/index.js
+++ back/index.js
@@ -1,6 +1,17 @@
 const express = require('express')
+const axios = require('axios')
 const app = express()
 const port = 3000
+
+
+
+axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?archetype=Dark%20Magician')
+  .then(response => {
+    console.log(response.data)
+  })
+  .catch(error => {
+    console.error(error)
+  })
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
