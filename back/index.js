4baf8839273a0e0a0dc68386331c65b450fb304d
Kaowarstail <victor.dane92@gmail.com>
2024-03-01 16:54:54 +0100

    gitignore

--- back/index.js
+++ back/index.js
@@ -1,38 +1,50 @@
 const express = require('express')
 const axios = require('axios')
 const app = express()
-const port = 3000
+const port = 3001
 
 
 
 function getArchetypeCards(archetype) {
   const url = `https://db.ygoprodeck.com/api/v7/cardinfo.php?archetype=${encodeURIComponent(archetype)}`;
 
-  axios.get(url)
+  return axios.get(url) // Retourner la Promesse
     .then(response => {
-      console.log(response.data);
+      return response.data;
     })
     .catch(error => {
       console.error(error);
     });
 }
+
+app.get('/archetype/:name', async (req, res) => { 
+  try {
+    const archetypeCards = await getArchetypeCards(req.params.name) 
+  } catch (error) {
+    res.status(500).send({ error: 'An error occurred' })
+  }
+})
 
 function getCardInfo(name) {
   const url = `https://db.ygoprodeck.com/api/v7/cardinfo.php?name=${encodeURIComponent(name)}`;
 
-  axios.get(url)
+  return axios.get(url)
     .then(response => {
-      console.log(response.data);
+      return response.data;
     })
     .catch(error => {
       console.error(error);
     });
 }
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/', async (req, res) => { 
+  try {
+    const cardInfo = await getCardInfo('Dark Magician') 
+    res.send(cardInfo)
+  } catch (error) {
+    res.status(500).send({ error: 'An error occurred' })
+  }
 })
-
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
