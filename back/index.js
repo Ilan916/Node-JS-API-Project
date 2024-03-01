cf8b974c180d510c5e8f3af53353181f58c2b318
Kaowarstail <victor.dane92@gmail.com>
2024-03-01 16:41:47 +0100

    skeleton call api

--- back/index.js
+++ back/index.js
@@ -5,13 +5,29 @@ const port = 3000
 
 
 
-axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?archetype=Dark%20Magician')
-  .then(response => {
-    console.log(response.data)
-  })
-  .catch(error => {
-    console.error(error)
-  })
+function getArchetypeCards(archetype) {
+  const url = `https://db.ygoprodeck.com/api/v7/cardinfo.php?archetype=${encodeURIComponent(archetype)}`;
+
+  axios.get(url)
+    .then(response => {
+      console.log(response.data);
+    })
+    .catch(error => {
+      console.error(error);
+    });
+}
+
+function getCardInfo(name) {
+  const url = `https://db.ygoprodeck.com/api/v7/cardinfo.php?name=${encodeURIComponent(name)}`;
+
+  axios.get(url)
+    .then(response => {
+      console.log(response.data);
+    })
+    .catch(error => {
+      console.error(error);
+    });
+}
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
